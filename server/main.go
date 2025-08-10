105044146aa2e9eb8bb6a1a8dba5709ccceb0bf6
Ana Alexandru-Gabriel <ana.alexandru.gabriel@proton.me>
2025-08-11 01:08:18 +0300

    core: working on MIME

--- server/main.go
+++ server/main.go
@@ -132,6 +132,7 @@ func fileUpload(httpWriter http.ResponseWriter, req *http.Request) {
 	for key,value := range req.MultipartForm.File {
 		fmt.Println(key, " : ")
 		for _, file_part := range value {
+			fmt.Prinln(file_part)
 			file, err := file_part.Open()
 			if err != nil {
 				fmt.Println(err)
